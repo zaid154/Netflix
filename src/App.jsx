047490e92b1557d9d8067cb59ddc29156698e051
Mohd Zaid <mohd.zaid.620268@gmail.com>
2026-05-02 21:47:42 +0530

    Second commit to Netflix

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-blue-500 text-white p-4">Welcome to React!</h1>
+    </div>
+  );
+}
+
+export default App;
